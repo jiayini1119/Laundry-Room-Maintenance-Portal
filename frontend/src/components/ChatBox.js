0bcd1846ccf20798948d2cdd15aae764f0bb5d20
Jiayi Ni <nijiayi1119@outlook.com>
2023-03-09 14:31:19 -0800

    Grouped the logic for accessing single chat for staff and students together

--- frontend/src/components/ChatBox.js
+++ frontend/src/components/ChatBox.js
@@ -1,48 +1,25 @@
 import Box from '@mui/material/Box';
 import SingleChat from "./SingleChat";
 import { ChatState } from "../Context/ChatProvider";
-import SingleChatStudent from './SingleChatStudent';
 
 const Chatbox = ({ fetchAgain, setFetchAgain }) => {
   const { selectedChat } = ChatState();
-  const email = localStorage.getItem('email')
-
-  if (email === "admin@admin.com"){
-    return (
-      <Box
-        sx={{
-          display: { base: selectedChat ? "flex" : "none", md: "flex" },
-          alignItems: "center",
-          flexDirection: "column",
-          p: 3,
-          bg: "white",
-          width: { base: "100%", md: "68%" },
-          borderRadius: "lg",
-          borderWidth: "1px",
-        }}
-      >
-        <SingleChat fetchAgain={fetchAgain} setFetchAgain={setFetchAgain} />
-      </Box>
-    );
+  return (
+    <Box
+      sx={{
+        display: { base: selectedChat ? "flex" : "none", md: "flex" },
+        alignItems: "center",
+        flexDirection: "column",
+        p: 3,
+        bg: "white",
+        width: { base: "100%", md: "68%" },
+        borderRadius: "lg",
+        borderWidth: "1px",
+      }}
+    >
+      <SingleChat fetchAgain={fetchAgain} setFetchAgain={setFetchAgain} />
+    </Box>
+  );
   }
-  else{
-    return (
-      <Box
-        sx={{
-          display: { base: selectedChat ? "flex" : "none", md: "flex" },
-          alignItems: "center",
-          flexDirection: "column",
-          p: 3,
-          bg: "white",
-          width: { base: "100%", md: "68%" },
-          borderRadius: "lg",
-          borderWidth: "1px",
-        }}
-      >
-        <SingleChatStudent fetchAgain={fetchAgain} setFetchAgain={setFetchAgain} />
-      </Box>
-    );
-  }
-};
 
 export default Chatbox;
